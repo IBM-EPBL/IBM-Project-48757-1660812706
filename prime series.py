97fb6aa4103330f5de75ac5798d83204820fb071
Vkkalaiyarasan <106609878+Vkkalaiyarasan@users.noreply.github.com>
2022-11-03 11:13:40 +0530

    Delete prime series.py

--- prime series.py
+++
@@ -1,9 +0,0 @@
-lower=int(input("Enter the min value :"))
-upper=int(input("Enter the max value :"))
-for a in range(lower,upper+1):
-    if a>1:
-       for i in range(2,a):
-           if (a%i)==0:
-              break
-       else:
-           print(a)
